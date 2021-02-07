a2381a0742041d1fe0293486df03a6811d4dd51f
Duy <duy_truong993@yahoo.com>
2021-02-07 00:50:16 -0800

    Update class

--- clsCommon.cs
+++ clsCommon.cs
@@ -6,6 +6,14 @@ namespace ConsoleApp1
 {
     static class clsCommon
     {
+        /// <summary>
+        /// The MidB function is used with byte data held within a string. 
+        /// </summary>
+        /// <param name="strInput">The string that you wish to extract from.</param>
+        /// <param name="index">The starting position of the substring.</param>
+        /// <param name="length">The length of the substring.</param>
+        /// <returns>Byte data contained in a string</returns>
+        /// Create : 2021/02/05
         public static string MidB(string strInput, int index, int length) 
         {
             if (string.IsNullOrEmpty(strInput))
@@ -19,10 +27,24 @@ namespace ConsoleApp1
             strInput = Encoding.GetEncoding("SJIS").GetString(byteArr);
             return strInput;
         }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="strInput">The string that you wish to extract from.</param>
+        /// <returns></returns>
         public static byte[] AscB(string strInput)
         {            
             return Encoding.ASCII.GetBytes(strInput);
         }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="strInput">The string that you wish to extract from.</param>
+        /// <param name="length">The length of the substring.</param>
+        /// <returns>Byte data contained in a string</returns>
+        /// Create : 2021/02/05
         public static string LeftB(string strInput, int length)
         {
             if (string.IsNullOrEmpty(strInput))
@@ -35,6 +57,14 @@ namespace ConsoleApp1
             strInput = Encoding.GetEncoding("SJIS").GetString(byteArr);
             return strInput;
         }
+
+        /// <summary>
+        /// The LEFT function allows you to extract a substring from a string, starting from the left-most character.
+        /// </summary>
+        /// <param name="strInput">The string that you wish to extract from.</param>
+        /// <param name="length">The length of the substring.</param>
+        /// <returns>A string/text value</returns>
+        /// Create : 2021/02/05
         public static string Left(string strInput, int length)
         {
             if (string.IsNullOrEmpty(strInput))
@@ -43,6 +73,15 @@ namespace ConsoleApp1
             }
             return strInput.Substring(length);
         }
+
+        /// <summary>
+        /// The MID function extracts a substring from a string (starting at any position).
+        /// </summary>
+        /// <param name="strInput">The string that you wish to extract from.</param>
+        /// <param name="index">The starting position of the substring.</param>
+        /// <param name="length">The length of the substring.</param>
+        /// <returns>A string/text value</returns>
+        /// Create : 2021/02/05
         public static string Mid(string strInput, int index, int length)
         {
             if (string.IsNullOrEmpty(strInput))
@@ -51,6 +90,13 @@ namespace ConsoleApp1
             }
             return strInput.Substring(index, length);
         }
+
+        /// <summary>
+        /// The RIGHT function extracts a substring from a string starting from the right-most character.
+        /// </summary>
+        /// <param name="strInput">The string that you wish to extract from.</param>
+        /// <param name="length">The length of the substring.</param>
+        /// <returns>A string/text value</returns>
         public static string Right(string strInput, int length)
         {
             if (string.IsNullOrEmpty(strInput))
@@ -59,6 +105,12 @@ namespace ConsoleApp1
             }
             return strInput.Substring(strInput.Length - length);
         }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="count"></param>
+        /// <returns>Created number </returns>
         public static string Space(int count)
         {
             string space = "";
@@ -70,7 +122,16 @@ namespace ConsoleApp1
                 }
             }
             return space;
-        }        
+        }
+
+        /// <summary>
+        /// Is the string a number ? 
+        /// true : number
+        /// false : not number
+        /// </summary>
+        /// <param name="strInput">The string that you wish to extract from.</param>
+        /// <returns>Is the string a number</returns>
+        /// Create : 2021/02/05
         public static bool IsNumber(string strInput)
         {
             bool result = true;
